0f9a54471f042a1d770ea434b037612b89c621b9
Satyam Singh <satyamrajput7239@gmail.com>
2024-07-05 10:48:14 +0530

    initial commit

---
+++ src/component/Form.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+const Form = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const handlerFirst = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handlerLast = (event) => {
+    setLastName(event.target.value);
+  };
+  const DisName = () => {
+    return <div>{`Full Name: ${firstName} ${lastName}`}</div>;
+  };
+  const handlerDisplayName = (e) => {
+    e.preventDefault();
+    if (!firstName) {
+      alert("Please enter first name");
+    } else if (!lastName) {
+      alert("Please enter last name");
+    } else {
+      setClicked(true);
+      DisName();
+    }
+  };
+  return (
+    <form onSubmit={handlerDisplayName}>
+      <h1>Full Name Display</h1>
+      <div>
+        <span>First Name:</span>
+        <input type="text" onChange={handlerFirst} value={firstName} />
+      </div>
+      <div>
+        <span>Last Name:</span>
+        <input type="text" onChange={handlerLast} value={lastName} />
+      </div>
+      <button type="submit">Submit</button>
+      {clicked ? <DisName /> : ""}
+    </form>
+  );
+};
+export default Form;
